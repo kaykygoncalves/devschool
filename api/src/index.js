828aa1f8cf7d52727819926dc9f7d9c064897622
kaykygoncalves <ra49557164824@acaonsfatima.org.br>
2021-09-09 18:24:24 -0300

    0909

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,20 @@
-ss
+import db from './db.js';
+import express from 'express'
+import cors from 'cors'
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/matricula', async (req, resp) => {
+    try {
+        let alunos = await db.tb_matricula.findAll({ order: [['id_matricula', 'desc']] });
+        resp.send(alunos);
+    } catch (e) {
+        resp.send({ erro: e .toString() })
+    }
+})
+
+
+app.listen(process.env.PORT,
+           x => console.log(`Server up at port ${process.env.PORT}`))
